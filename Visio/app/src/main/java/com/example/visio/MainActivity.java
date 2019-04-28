6a4220c392518ffb1211af96224aea99d5d5ce7f
BogdanDenis <denys.bohdan@nure.ua>
2019-04-28 04:50:11 +0300

    opencv camera + recognizing

--- Visio/app/src/main/java/com/example/visio/MainActivity.java
+++ Visio/app/src/main/java/com/example/visio/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextPaint;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -18,7 +19,18 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
@@ -27,19 +39,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener {
     private Camera mCamera;
     private CameraPreview mPreview;
     private Classificator classificator;
+    private JavaCameraView javaCameraView;
+    private Mat mRgba;
+    private Timestamp lastRecognitionTime = new Timestamp(new Date().getTime());
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS: {
+                    javaCameraView.enableView();
+                    break;
+                }
+                default: {
+                    super.onManagerConnected(status);
+                    break;
+                }
+
+            }
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -49,51 +84,90 @@ public class MainActivity extends AppCompatActivity {
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
-        mCamera = getCameraInstance();
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+        javaCameraView = (JavaCameraView)findViewById(R.id.camera_preview);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
 
         try {
             classificator = new Classificator(loadModelFile(MainActivity.this, "model.tflite"), loadLabels());
-
-            Button btn = (Button)findViewById(R.id.button_capture);
-            btn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FrameLayout view = (FrameLayout)findViewById(R.id.camera_preview);
-                    view.setDrawingCacheEnabled(true);
-                    view.buildDrawingCache();
-                    Bitmap bitmap = view.getDrawingCache();
-
-                    String res = classificator.classify(bitmap);
-
-                    TextView textView = (TextView)findViewById(R.id.textView);
-                    textView.setText(res);
-                }
-            });
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        if (OpenCVLoader.initDebug()) {
-            Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
         }
     }
 
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        } catch (Exception e) {
-            e.getMessage();
-            // Camera is not available (in use or does not exist)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
         }
-        return c; // returns null if camera is unavailable
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_SHORT).show();
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_SHORT).show();
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, mLoaderCallback);
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat frame) {
+        mRgba.release();
+        mRgba = frame;
+
+        if (canRunRecognition()) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Bitmap bitmap = Bitmap.createBitmap(mRgba.width(), mRgba.height(), Bitmap.Config.ARGB_8888);
+
+                    Utils.matToBitmap(mRgba, bitmap);
+
+                    String res = classificator.classify(bitmap);
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(res);
+                }
+            });
+
+            lastRecognitionTime = new Timestamp(new Date().getTime());
+        }
+
+        return mRgba;
+    }
+
+    private boolean canRunRecognition() {
+        Timestamp currentTime = new Timestamp(new Date().getTime());
+
+        return currentTime.getTime() - lastRecognitionTime.getTime() > 500;
     }
 
     private MappedByteBuffer loadModelFile(Activity activity, String modelFile) throws IOException {
